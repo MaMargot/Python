3ea122a7213dd85bd8961fb0ffafaf8b91186b3b
machuangjie <923172042@qq.com>
2018-09-13 10:32:25 +0800

    2018.9.13

--- Python学习笔记.py
+++ Python学习笔记.py
@@ -1145,6 +1145,49 @@ tidy.stdin.write(text)
 tidy.stdin.close()
 print tidy.stdout.read()
 #使用HTMLOASESER模块屏幕抓取程序
+from HTMLParser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        """
+        recognize start tag, like <div>
+        :param tag:
+        :param attrs:
+        :return:
+        """
+        print("Encountered a start tag:", tag)
+    def handle_endtag(self, tag):
+        """
+        recognize end tag, like </div>
+        :param tag:
+        :return:
+        """
+        print("Encountered an end tag :", tag)
+    def handle_data(self, data):
+        """
+        recognize data, html content string
+        :param data:
+        :return:
+        """
+        print("Encountered some data  :", data)
+    def handle_startendtag(self, tag, attrs):
+        """
+        recognize tag that without endtag, like <img />
+        :param tag:
+        :param attrs:
+        :return:
+        """
+        print("Encountered startendtag :", tag)
+    def handle_comment(self,data):
+        """
+        :param data:
+        :return:
+        """
+        print("Encountered comment :", data)
+parser = MyHTMLParser()
+parser.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me!</h1><img src = "" />'
+            '<!-- comment --></body></html>')
+
 from urllib import urlopen
 from HTMLParser import HTMLParser
 class scraper(HTMLParser):
@@ -1172,6 +1215,8 @@ text=urlopen('http://python.org/community/jobs').read()
 parser=Scraper()
 parser.feed(text)
 parser.close()
+
+
 
 
 
