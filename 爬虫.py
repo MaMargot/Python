61b07861e80110d5ea3563c74b9558a3cbcba12c
machuangjie <923172042@qq.com>
2018-09-18 09:35:22 +0800

    2018.9.18

--- 爬虫.py
+++ 爬虫.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2018/9/17 16:18
+# @Time    : 2018/9/18 9:21
 # @Author  : Ryu
 # @Site    : 
 # @File    : 爬虫.py
 # @Software: PyCharm
+
+import bs4
+import urllib
+html=urllib.urlopen("http://www.pythonscraping.com/pages/page1.html")
+print(html.read())
+bsObj=bs4.BeautifulSoup(html.read())
+print(bsObj.h1)
