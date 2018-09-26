5855049de02890e19be5427ab9d84e9795e2cde0
machuangjie <923172042@qq.com>
2018-09-26 10:49:29 +0800

    2018.9.26

---
+++ 分析.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/26 10:42
+# @Author  : Ryu
+# @Site    : 
+# @File    : 分析.py
+# @Software: PyCharm
+
+#NumPy库,高性能科学计算和数据分析的基础包，目的在于更高效地辅助使用数据分析功能
+#ndarray是一个通用的同构数据多维容器,其中shape表示各维度的数组，dtype用于说明数组数据类型的对象
+import numpy as np
+data1=[6,7.5,8,1,0]
+arr1=np.array(data1)
+
+
+
+
