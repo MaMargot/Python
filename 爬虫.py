8bfbd25370f35c64fb5b7cf0c4a9893663c8788e
machuangjie <923172042@qq.com>
2018-10-12 18:17:16 +0800

    2018.10.12

--- 爬虫.py
+++ 爬虫.py
@@ -631,8 +631,55 @@ print (dictReader.fieldnames)
 for row in dictReader:
     print(row)
 
+#PDF文件解析
+from urllib import urlopen
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from io import open
+def readPDF(pdfFile):
+    rsrcmgr=PDFResourceManager()
+    retstr=StringIO()
+    laparams=LAParams()
+    device=TextConverter(rsrcmgr,retstr,laparams=laparams)
+    process_pdf(rsrcmgr,device,pdfFile)
+    device.close()
+    content=retstr.getvalue()
+    retstr.close()
+    return content
+pdfFile=urlopen("http://pythonscraping.com/pages/warandpeace/chapter1.pdf")
+outputString=readPDF(pdfFile)
+print(outputString)
+pdfFile.close()
 
-
+#.doc与.docx文件
+from zipfile import ZipFile
+from urllib import urlopen
+from io import BytesIO
+from bs4 import BeautifulSoup
+wordFile=urlopen("http://pythonscraping.com/pages/AWordDocument.docx").read()
+wordFile=BytesIO(wordFile)
+document=ZipFile(wordFile)
+xml_content=document.read('word/document.xml')#document中包含多个文件，访问的是word文件夹中文件
+print (xml_content.decode('utf-8'))
+wordobj=BeautifulSoup(xml_content.decode('utf-8'))
+textStrings=wordobj.findAll("w:t")
+for textElem in textStrings:
+    print (textElem.text)
+for textElem in textStrings:
+    closeTag=" "
+    try:
+        style=textElem.parent.previousSibling.find("w:pstyle")
+        if style is not None and style["w:val"]=="Title":
+            print("<h1>")
+            closeTag="</h1>"
+    except AttributeError:
+        #不打印标签
+        pass
+    print(textElem.text)
+    print(closeTag)
 
 
 
